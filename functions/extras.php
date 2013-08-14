071310e1107930bc446d30273c2fb7b2dede7cfc
Adam Walter <adam@adamwalter.com>
2013-08-14 11:22:38 -0400

    Adds breadcrumbs function

--- functions/extras.php
+++ functions/extras.php
@@ -129,4 +129,35 @@
 		return $params;
 	}
 	add_filter( 'dynamic_sidebar_params', 'kc_dynamic_sidebar_params' );
+
+
+	// Custom Breadcrumbs
+	function vtl_page_breadcrumbs() {
+		$delimiter = ':';
+		$currentBefore = '<span class="current">';
+		$currentAfter = '</span>';
+		if ( !is_home() && !is_front_page() || is_paged() ) {
+		echo '<div class="breadcrumbs">';
+		global $post;
+		if ( is_page() && !$post->post_parent ) {
+			echo $currentBefore;
+			the_title();
+			echo $currentAfter; }
+		elseif ( is_page() && $post->post_parent ) {
+			$parent_id	= $post->post_parent;
+			$breadcrumbs = array();
+			while ($parent_id) {
+			$page = get_page($parent_id);
+			$breadcrumbs[] = '<a href="' . get_permalink($page->ID) . '">' . get_the_title($page->ID) . '</a>';
+			$parent_id	= $page->post_parent;
+			}
+			$breadcrumbs = array_reverse($breadcrumbs);
+			foreach ($breadcrumbs as $crumb) echo $crumb . ' ' . $delimiter . ' ';
+			echo $currentBefore;
+			the_title();
+			echo $currentAfter;
+		}
+		echo '</div>';
+		}
+	}
 ?>
