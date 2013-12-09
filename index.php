dcd69010ec7c54412f8f84a24f271ea8530637cc
Adam Walter <adam@adamwalter.com>
2013-12-09 12:41:24 -0500

    Conditional class on sticky posts

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-	<article id="post-<?php the_ID(); ?>">
+	<article id="post-<?php the_ID(); ?>" <?php if(is_sticky()) { echo 'class="sticky-post"';}?>>
 		<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 		<p class="date"><?php human_friendly_date(); ?></p>
 		<?php
