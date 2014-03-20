a56276365448d4c569af3608a658a28d859b0a72
Adam Walter <adam@adamwalter.com>
2014-03-20 11:58:26 -0400

    Enqueue Foundation

--- functions.php
+++ functions.php
@@ -12,6 +12,7 @@
 
         wp_enqueue_style( 'reset', get_template_directory_uri() . '/css/reset.css', '1.0', 'screen' );
         wp_enqueue_style( 'fonts', get_template_directory_uri() . '/css/fonts.css', '1.0', 'screen' );
+        //wp_enqueue_style( 'foundation-grid', get_template_directory_uri() . '/css/foundation-grid.css', '4.0', 'screen' );
         wp_enqueue_style( 'screen', get_stylesheet_uri(), '', '1.0', 'screen' );
 
         if ( is_singular() ) {
