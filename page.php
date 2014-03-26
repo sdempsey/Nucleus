c2116422115aa25646a040bc1f3e7796d32c07b0
Adam Walter <adam@adamwalter.com>
2014-03-26 17:49:22 -0400

    Remove old comments include

--- page.php
+++ page.php
@@ -2,7 +2,7 @@
 
     <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
         <?php the_content(); ?>
-        <?php if ( comments_open() || '0' != get_comments_number() ) comments_template( '', true ); ?>
+        <?php comments_template('', true); ?>
     <?php endwhile; ?>
 
 <?php get_footer(); ?>
