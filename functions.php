b0becaadf191ea44e9dc49d879ff63f5df9d0fb2
Adam Walter <adam@adamwalter.com>
2014-04-16 12:53:51 -0400

    More font options, and in REMs because that's okay here.

--- functions.php
+++ functions.php
@@ -189,7 +189,7 @@
         $options['theme_advanced_fonts'] = 'Helvetica=Helvetica Neue, Helvetica, Arial, sans-serif;'.
                                            'Custom Font=Custom Font, Helvetica Neue, Helvetica, Arial, sans-serif;'.
                                            '';
-        $options['theme_advanced_font_sizes'] = '0.75em,0.875em,1em,1.125em,1.25em,1.5em,1.75em,2em';
+        $options['theme_advanced_font_sizes'] = '0.5rem,0.75rem,0.875rem,1rem,1.125rem,1.25rem,1.375rem,1.5rem,1.75rem,1.875rem,2rem';
         $options['theme_advanced_text_colors'] = 'ffffff,ba192c,20b7dd';
 
         return $options;
