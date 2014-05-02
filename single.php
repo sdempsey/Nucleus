48302b57f1e30e887b8f50f89cc607dd2130ff2f
Adam Walter <adam@adamwalter.com>
2014-05-01 22:34:52 -0400

    Remove author meta which we never use

--- single.php
+++ single.php
@@ -4,6 +4,12 @@
         <article id="post-<?php the_ID(); ?>">
             <h1><?php the_title(); ?></h1>
             <?php the_content(); ?>
+
+            <?php if ( get_the_author_meta( 'description' ) ) : ?>
+                <?php echo get_avatar( get_the_author_meta( 'user_email' ) ); ?>
+                <h2>About <?php echo get_the_author() ; ?></h2>
+                <?php the_author_meta( 'description' ); ?>
+            <?php endif; ?>
         </article>
 
         <?php comments_template('', true); ?>
