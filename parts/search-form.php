b4add08b0d751464da1f95089bf051559f27191f
Adam Walter <adam@adamwalter.com>
2014-07-17 09:27:19 -0400

    Replace depreciated code

--- parts/search-form.php
+++ parts/search-form.php
@@ -1,4 +1,4 @@
-<form method="get" id="searchform" class="searchform" action="<?php bloginfo('siteurl'); ?>" role="search">
+<form method="get" id="searchform" class="searchform" action="<?php echo home_url(); ?>" role="search">
     <label for="s" class="screen-reader-text"><?php _ex( 'Search', 'assistive text', '_s' ); ?></label>
     <input type="search" class="field" name="s" value="<?php echo esc_attr( get_search_query() ); ?>" id="s" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', '_s' ); ?>" />
     <input type="submit" class="submit" id="searchsubmit" value="<?php echo esc_attr_x( 'Search', 'submit button', '_s' ); ?>" />
