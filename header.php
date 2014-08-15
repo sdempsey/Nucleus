d3e5786a2650d47cf3e1c6099bfaea9742d12012
Adam Walter <adam@adamwalter.com>
2014-08-15 12:37:37 -0400

    Add Paul Irish 'no-js' class technique

--- header.php
+++ header.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+<html <?php language_attributes(); ?> class="no-js">
 <head>
 
-    <title><?php ( is_front_page() ? wp_title() : wp_title( '|', true, 'right' ) ) ?></title>
+    <script>(function(H){H.className=H.className.replace(/\bno-js\b/,'js');})(document.documentElement);</script>
 
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title><?php ( is_front_page() ? wp_title() : wp_title( '|', true, 'right' ) ) ?></title>
 
     <?php wp_head(); ?>
 
--- scripts/site/main.js
+++ scripts/site/main.js
@@ -3,7 +3,5 @@ jQuery(document).ready(function($) {
     // Add the_title(); as an alt attribute to any images without one. This is for SEO.
     //$('body').find('img:not([alt])').attr('alt', SiteInfo.the_awesome_title);
 
-    // Remove no-js class on HTML element (Keep this at the end)
-    $('html').addClass('js-ready');
 
 });
