32138407534e88783f202bbdf7a9ae201d6aaa97
Adam Walter <adam@adamwalter.com>
2014-09-08 12:48:28 -0400

    Delete seo.php

--- functions.php
+++ functions.php
@@ -1,5 +1,4 @@
 <?php
-    require_once locate_template('/functions/seo.php');
     require_once locate_template('/functions/admin.php');
     require_once locate_template('/functions/extras.php');
     require_once locate_template('/functions/cleanup.php');
